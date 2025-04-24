9b9210c0853f79ae6398e8fe5d6f4dba0e7d45a7
jithanroy <jithanroyjony@gmail.com>
2025-04-25 03:24:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegistrationPage from "./pages/RegistrationPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route element={<HomePage />} path="/" exact />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<ProfilePage />} path="/me" />
+        <Route element={<RegistrationPage />} path="/register" />
+
+        <Route element={<NotFoundPage />} path="*" />
+      </Routes>
+    </>
+  )
+}
+
+export default App
